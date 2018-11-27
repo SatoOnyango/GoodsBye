5473f1dc040aec235ae1c4d721e37b57306cdb8a
佐藤一馬 <>
2018-11-27 12:22:08 +0800

    カラム追加

--- GoodsBye.sql
+++ GoodsBye.sql
@@ -50,6 +50,7 @@ CREATE TABLE `items` (
   `comment_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
+  `deadline` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
