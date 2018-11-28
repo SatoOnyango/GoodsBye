914bb58aea57c3cf3a4264f2ee62dc08da636e42
shinyahirai <908.shinya@gmail.com>
2018-11-28 11:46:39 +0800

    templateの紹介

---
+++ hoge.md
@@ -0,0 +1,17 @@
+# タイトル
+## ほげほげ
+
+hoge *hoge*
+
+```php
+echo 'Hello world dayo';
+```
+
+| Left align | Right align | Center align |
+|:-----------|------------:|:------------:|
+| This       | This        | This         |
+| column     | column      | column       |
+| will       | will        | will         |
+| be         | be          | be           |
+| left       | right       | center       |
+| aligned    | aligned     | aligned      |
---
+++ template.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <!-- 
+    CSSの読み込み順
+    ①Bootstrap
+    ②Font Awesome
+    ③独自CSS
+   -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/hoge.css">
+</head>
+<body>
+  
+  <!-- 
+    JSの読み込み順
+    ①jQuery
+    ②jQuery-migrate
+    ③bootstrap
+    ④独自JS
+   -->
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
