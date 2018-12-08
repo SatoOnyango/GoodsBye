cc4e50c9dc88224784340b0178dc0399121a6e45
まりあ <morishitamaria@morishitamarianoMacBook-puro.local>
2018-12-08 18:11:16 +0800

    no message

--- main.php
+++ main.php
@@ -1,14 +1,118 @@
 <?php
 session_start();
 require('dbconnect.php');
-const CONTENT_PER_PAGE = 10;
 
-// if (!isset($_SESSION['GoodsBye']['id'])) {
-//     header('Location: signin.php');
-//     exit();
-// }
+$errors = [];
+
+if (isset($_GET['action']) && $_GET ['action'] == 'rewrite'){
+    $_POST['input_content'] = $_SESSION['GoodsBye']['content'];
+    $_POST['input_item_img'] = $_SESSION['GoodsBye']['item_img'];
+
+    $errors['rewrite'] = true;
+}
 
 
+$content = '';
+
+$item_img = '';
+
+var_dump($_POST);
+
+if (!empty($_POST)) {
+    echo 'a';
+    $content = $_POST['input_content'];
+
+    $item_img = $_POST['input_item_img'];
+
+    if ($content == '') {
+        $errors['content'] = 'blank';
+        echo 'b';
+    }
+
+    if ($item_img == '') {
+        $errors['item_img'] = 'blank';
+    }
+
+    $file_name = '';
+
+    if (!isset($_GET['action'])) {
+
+        $file_name = $_FILES['input_img_name']['name'];
+    }
+    if (!empty($file_name)) {
+
+        $file_type = substr($file_name,-3);
+
+
+        $file_type = strtolower($file_type);
+
+
+        if ($file_type != 'png' && $file_type != 'jpg'&& $file_type !='gif'&& $file_type != 'jpeg') {
+
+            $errors['img_name'] = 'type';
+
+        }
+
+        if (empty($errors)) {
+            echo "入力不備なし！<br>";
+            $date_str = date('YmdHis');
+            $submit_file_name = $date_str . $file_name;
+
+
+            move_uploaded_file($_FILES['input_img_name']['tmp_name'],'../user_profile_img/' . $submit_file_name);
+
+
+            $_SESSION['GoodsBye']['content'] = $content;
+            $_SESSION['GoodsBye']['item_img'] = $item_img;
+
+
+            header('Location: main.php');
+            exit();
+        }
+    }
+
+    if (!empty($_POST)) {
+      $item_img = $_POST['item_img'];
+
+      if ($item_img !='') {
+        $sql = 'INSERT INTO `items`(`content`,`item_img`)VALUES(?,?)';
+
+        $data = [$content,$item_img['user_id']];
+
+        $stmt = $dbh->prepare($data);
+
+        $stmt->execute($data);
+
+        header('Location: main.php');
+        exit();
+      }else{
+        $errors['content'] = 'blank';
+
+      }
+      
+    }
+
+$sql = 'SELECT `f`.*,`u`.`name`,`u`.`img_name`FROM`feeds` AS`f` LEFT JOIN`users` AS `u` ON`f`.`user_id` = `u`.`id` ORDER BY`f`.`created` DESC LIMIT '.CONTENT_PER_PAGE.' OFFSET '.$start;
+
+
+$data = [];
+}
+
+
+
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    $items = [];
+    while (true) {
+
+      $record = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($record == false) {
+        break;
+      }
+    }
+}
 
 
 
@@ -18,113 +122,112 @@ const CONTENT_PER_PAGE = 10;
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>GoodsBye</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <meta charset="utf-8">
+  <title>GoodsBye</title>
+  <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet"> -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
 </head>
 <body>
-    <?php include('navbar.php'); ?>
-    <div class="container">
-        <div class="row">
-            <div>
-                <h1></h1><br>
-            </div>
-            <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <h1 class="goodsbye-title">GoodsBye</h1>
-            </div>
+  <?php // include('navbar.php'); ?>
+  <div class="container">
+    <div class="row">
+      <div>
+        <h1></h1><br>
+      </div>
+      <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h1 class="goodsbye-title">GoodsBye</h1>
+      </div>
 
-            <!-- １行目×３グッズ row1 -->
-            <div class="row">
-                <!-- TH1 -->
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                      <a href="detail.php?item_id=1" class="">
-                        <div class="caption">
-                             <h4 class="">COMMENT</h4>
-                            <p class=""></p>
-                        </div>
-                        <img src="user_profile_img/default.png" alt="..." class="thumbnail">
-                       </a>
-                    </div>
-                </div>
-            </a>
+      <!-- １行目×３グッズ row1 -->
+  <div class="row">
+      <!-- TH1 -->
+      <div class="col-sm-4">
+          <div class="thumbnail">
+            <a href="detail.php?item_id=1" class="">
+              <div class="caption">
+
+                  <p class=""></p>
+              </div>
+              <img src="user_profile_img/default.png" alt="..." class="thumbnail">
+             </a>
+          </div>
+
+      </div>
+  </a>
 
                 <!-- TH2 -->
 
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                      <a href="detail.php?item_id=2" class="">
-                        <div class="caption">
-                             <h4 class="">COMMENT</h4>
-                            <p class=""></p>
-                        </div>
-                        <img src="user_profile_img/petbotles.jpeg" alt="..." class="thumbnail">
-                        </p>
-                    </div>
-                </div>
+      <div class="col-sm-4">
+          <div class="thumbnail">
+            <a href="detail.php?item_id=2" class="">
+              <div class="caption">
+
+                  <p class=""></p>
+              </div>
+              <img src="user_profile_img/petbotles.jpeg" alt="..." class="thumbnail">
+              </p>
+          </div>
+      </div>
 
 
                 <!-- TH3 -->
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                        <a href="detail.php?item_id=3" class="">
-                        <div class="caption">
-                            <h4 class="">COMMENT</h4>
-                            <p class=""></p>
-                        </div>
-                        <img src="user_profile_img/test_signin_user_img.jpg" alt="..." class="thumbnail ">
-                        </a>
-                    </div>
-                </div>
-            </div>
+      <div class="col-sm-4">
+          <div class="thumbnail">
+              <a href="detail.php?item_id=3" class="">
+              <div class="caption">
+
+                  <p class=""></p>
+              </div>
+              <img src="user_profile_img/test_signin_user_img.jpg" alt="..." class="thumbnail ">
+              </a>
+          </div>
+      </div>
+  </div>
             <!-- end/row1 -->
 
         <!-- ２行目×３グッズ row2 -->
-        <div class="row">
-            <!-- TH4 -->
-            <div class="col-sm-4">
-                <div class="thumbnail">
-                        <a href="detail.php?item_id=4" class="">
-                    <div class="caption">
-                        <h4 class="">COMMENT</h4>
-                        <p class=""></p>
-                    </div>
-                    <img src="http://placehold.it/350x250" alt="..." class="">
-                    </a>
-                </div>
+<div class="row">
+    <!-- TH4 -->
+    <div class="col-sm-4">
+        <div class="thumbnail">
+                <a href="detail.php?item_id=4" class="">
+            <div class="caption">
+
+                <p class=""></p>
             </div>
+            <img src="user_profile_img/IMG_1380.JPG" alt="..." class=""> </a>
+        </div>
+    </div>
 
             <!-- TH5 -->
-            <div class="col-sm-4">
-                <div class="thumbnail">
-                        <a href="detail.php?item_id=5" class="">
-                    <div class="caption">
-                        <h4 class="">COMMENT</h4>
-                        <p class="">
-                        </p>
-                    </div>
-                    <img src="http://placehold.it/350x250" alt="..." class="">
-                </a>
-                </div>
+    <div class="col-sm-4">
+        <div class="thumbnail">
+                <a href="detail.php?item_id=5" class="">
+            <div class="caption">
+
+                <p class="">
+                </p>
             </div>
+            <img src="http://placehold.it/350x250" alt="..." class="">
+        </a>
+        </div>
+    </div>
 
 
             <!-- TH6 -->
-            <div class="col-sm-4">
-                <div class="thumbnail">
-                        <a href="detail.php?item_id=6" class="">
-                    <div class="caption">
-                        <h4 class="">COMMENT</h4>
-                        <p class="">
-                        </p>
-                    </div>
-                    <img src="http://placehold.it/350x250" alt="..." class="">
-                </a>
-                </div>
+    <div class="col-sm-4">
+        <div class="thumbnail">
+                <a href="detail.php?item_id=6" class="">
+            <div class="caption">
+
+                <p class="">
+                </p>
             </div>
+            <img src="http://placehold.it/350x250" alt="..." class="">
+        </a>
+        </div>
+    </div>
         </div>
         <!-- end/row2 -->
 
@@ -137,16 +240,21 @@ const CONTENT_PER_PAGE = 10;
                                 <form method="POST" action="">
                                     <div class="form-group" style="margin-bottom: 0px;">
                                         <textarea name="feed" class="form-control" rows="2" placeholder="Your Comment Here" style="font-size: 24px; text-align: center;"></textarea><br>
-                                    <?php if(isset($errors['feed']) && $errors['feed'] == 'blank'): ?>
-                                        <p class="text-danger">投稿を入力してください</p>
-                                    <?php endif; ?>
+                                        <value="<?php echo htmlspecialchars($content);?>">
+
+                                    <?php if(isset($errors['content']) && $errors['content'] == 'blank'): ?>
+                                        <p class="text-danger">文字を入力してください</p>         <?php endif; ?>
                                     </div>
                                     <div class="form-group">
                                         <label for="img_name">Your Googs Image</label>
-                                        <input type="file" name="input_img_name" id="img_name" accept="image/*"><!-- accept="image/*"画像以外選択できない -->
-                                        <?php if(isset($errors['img_name']) && $errors['img_name']== 'type'):?>
-                                        <p class ="text-danger">拡張子が違います</p>
-                                        <?php endif ;?>
+                                        <input type="file" name="input_img_name" id="img_name" accept="image/*">
+
+
+
+                                    <?php if (isset($errors['item_img'])&& $errors
+                                    ['item_img'] == 'blank'):?>
+                                        <p class="text-danger">写真を選択してください</p>
+                        <?php endif; ?> 
                                     </div>
                                     <input type="submit" value="POST (投稿する)" class="btn btn-primary">
                                 </form>
@@ -162,6 +270,9 @@ const CONTENT_PER_PAGE = 10;
 
         </div>
     </div>
+    
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js">選択する</script>
 </body>
 </html>
 
