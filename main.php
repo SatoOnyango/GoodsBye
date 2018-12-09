79287d9b60f9a066aa1a8fadc1b33d5da47dd987
tomoki sato <tomoki.akinori@gmail.com>
2018-12-09 15:31:44 +0800

    バリデーション追加

--- main.php
+++ main.php
@@ -1,87 +1,112 @@
 <?php
 session_start();
 require('dbconnect.php');
-
-   $errors = [];
-
-if (isset($_GET['action']) && $_GET ['action'] == 'rewrite'){
-    $_POST['input_content'] = $_SESSION['GoodsBye']['content'];
-    $_POST['input_item_img'] = $_SESSION['GoodsBye']['item_img'];
-
-    $errors['rewrite'] = true;
-
+// 1ページあたりの表示件数
+const CONTENT_PER_PAGE = 30;
+//ログインしてない状態でアクセス禁止
+if(!isset($_SESSION['GoodsBye']['id'])){
+   header('Location:signin.php');
+   exit();
 }
 
+//サインインユーザーの読み出し
+$sql='SELECT *FROM`users`WHERE`id`=?';
+$data=[$_SESSION['GoodsBye']['id']];
+$stmt=$dbh->prepare($sql);
+$stmt->execute($data);
+$signin_user=$stmt->fetch(PDO::FETCH_ASSOC);
 
+$errors = [];
 $content = '';
 
-$item_img = '';
-
-if (!empty($_POST)) {
-    
-$content = $_POST['input_content'];
-
-$item_img = $_POST['input_item_img'];
-
-if ($content == '') {
-    $errors['content'] = 'blank';
-}
-
-if ($item_img == '') {
-    $errors['item_img'] = 'blank';
-}
-
-        $file_name = '';
-
-       if (!isset($_GET['action'])) {
-
+//アイテム投稿
+if (!empty($_POST)){
+    // 商品説明があるか
+    $content =$_POST['content'];
+    if ($content == '') {
+        $errors['content'] = 'blank';
+    }
+    $file_name = '';
+    // 写真が選択されてか
+    if (isset($_FILES['input_img_name'])) {
         $file_name = $_FILES['input_img_name']['name'];
     }
-        if (!empty($file_name)) {
-
-            $file_type = substr($file_name,-3);
-
-
-            $file_type = strtolower($file_type);
-
-
-           if ($file_type != 'png' && $file_type != 'jpg'&& $file_type !='gif'&& $file_type != 'jpeg') {$errors['img_name'] = 'type';
-
-            }
-
-        if (empty($errors)) {
-        echo "入力不備なし！<br>";
-         
-
-}
-}
-
+    // echo'<pre>';
+    // var_dump($content);
+    // echo'</pre>';
+    // echo'<pre>';
+    // var_dump($file_name);
+    // echo'</pre>';
+    if (!empty($file_name)) {
+        $file_type = substr($file_name, -3);
+        $file_type = strtolower($file_type);
+        // 3. jpg,png,gifと比較し、当てはまらない場合$errors['img_name']に格納
+        if ($file_type != 'png' && $file_type != 'jpg' && $file_type != 'gif') {
+            $errors['input_img_name'] = 'type';
+        }
+    } else {
+        $errors['input_img_name'] = 'blank';
+    }
+    //アイテム投稿時エラーがなければデータベースに登録する
+    if (empty($errors)) {
         $date_str = date('YmdHis');
         $submit_file_name = $date_str . $file_name;
+        move_uploaded_file($_FILES['input_img_name']['tmp_name'],'user_profile_img/' . $submit_file_name);
+        $file_name=$submit_file_name;
 
+        $sql='INSERT INTO`items`(`content`,`item_img`,`user_id`,`created`)VALUES(?,?,?,NOW());';
+        $data= [$content,$file_name,$signin_user['id']];
+        $stmt=$dbh->prepare($sql);
+        $stmt->execute($data);
+// die();
+        header('Location:main.php');
+    }
+}
+// echo'<pre>';
+// var_dump($content);
+// echo'</pre>';
+// echo'<pre>';
+// var_dump($file_name);
+// echo'</pre>';
 
-        move_uploaded_file($_FILES['input_img_name']['tmp_name'],'../user_profile_img/' . $submit_file_name);
-
-
-        $_SESSION['GoodsBye']['content'] = $content;
-        $_SESSION['GoodsBye']['item_img'] = $item_img;
-
-
-        header('Location: main.php');
-        exit();
-
-
-        
-
-
-
-
+//アイテム投稿情報(ユーザー情報含む)をすべて取得
+$sql = 'SELECT `i`.*, `u`.`name` FROM `items` AS `i` LEFT JOIN `users` AS `u` ON `i`.`user_id` = `u`.`id`';
+$data = [];
+$stmt = $dbh->prepare($sql);
+$stmt->execute($data);
+// 投稿情報全てを入れる配列定義
+$contents=[];
+while(true){
+    $record= $stmt->fetch(PDO::FETCH_ASSOC);
+    if($record==false){
+    break;
+    }
+    $contents[] = $record;
 }
 
-
-
-
-
+if (isset($_GET['page'])) {
+    // ページの指定がある場合
+    $page = $_GET['page'];
+} else {
+    // ページの指定がない場合(初期値)
+    $page = 1;
+}
+// -1などの不正な値を渡された際の対策
+$page = max($page, 1);
+// feedsテーブルのレコード数を取得する
+// COUNT() 何レコードあるか集計するSQLの関数
+$sql = 'SELECT COUNT(*) AS `cnt` FROM `items`';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$cnt = $result['cnt'];
+// 最後のページ数を取得
+// 最後のページ = 取得したページ数 ÷ 1ページあたりのページ数
+$last_page = ceil($cnt / CONTENT_PER_PAGE);
+// 最後のページより大きい値を渡された際の対策
+$page = min($page, $last_page);
+// スキップするレコード数 = (指定ページ - 1) * 表示件数
+$start = ($page - 1) * CONTENT_PER_PAGE
 
 ?>
 
@@ -106,139 +131,116 @@ if ($item_img == '') {
                 <h1 class="goodsbye-title">GoodsBye</h1>
             </div>
 
-            <!-- １行目×３グッズ row1 -->
-            <div class="row">
-                <!-- TH1 -->
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                      <a href="detail.php?item_id=1" class="">
-                        <div class="caption">
+                <div class="row">
+                    <?php foreach($contents as $content): ?>
+                    <!-- TH1 -->
+                    <div class="col-sm-4">
+                        <div class="thumbnail">
+                          <a href="detail.php?item_id=<?php echo$content['id'];?>" class="">
+                            <div class="caption">
 
-                            <p class=""></p>
+                                <p class=""></p>
+                            </div>
+                            <img src="user_profile_img/<?php echo $content['item_img'];?>" alt="..." class="thumbnail">
+                          </a>
+                          <?php if(empty($content)):?>
+                            <img src="user_profile_img/petbotles.jpeg" alt="..." class="thumbnail">
+                          <?php endif;?>
+                           <?php if($signin_user['id']==$content['user_id']):?>
+                                    <a href="edit.php?item_id2=<?php echo$content['id'];?>" class="btn btn-success btn-xs">編集</a>
+                                    <a onclick="return confirm('ほんとに消すの？');" href="delete.php" class="btn btn-danger btn-xs">削除</a>
+                                    <!-- get送信時はURL?(キー＝値)=パラメーター -->
+                           <?php endif;?>
+    <!--                   <?php 
+                           // echo'<pre>';
+                           // var_dump($signin_user['id']);
+                           // echo'</pre>';
+
+                           // echo'<pre>';
+                           // var_dump($content['user_id']);
+                           // echo'</pre>';
+                           ?> -->
                         </div>
-                        <img src="user_profile_img/default.png" alt="..." class="thumbnail">
-                       </a>
                     </div>
-
-                </div>
-            </a>
-
-                <!-- TH2 -->
-
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                      <a href="detail.php?item_id=2" class="">
-                        <div class="caption">
-
-                            <p class=""></p>
-                        </div>
-                        <img src="user_profile_img/petbotles.jpeg" alt="..." class="thumbnail">
-                        </p>
-                    </div>
-                </div>
-
-
-                <!-- TH3 -->
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                        <a href="detail.php?item_id=3" class="">
-                        <div class="caption">
-
-                            <p class=""></p>
-                        </div>
-                        <img src="user_profile_img/test_signin_user_img.jpg" alt="..." class="thumbnail ">
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <!-- end/row1 -->
-
-        <!-- ２行目×３グッズ row2 -->
-        <div class="row">
-            <!-- TH4 -->
-            <div class="col-sm-4">
-                <div class="thumbnail">
-                        <a href="detail.php?item_id=4" class="">
-                    <div class="caption">
-
-                        <p class=""></p>
-                    </div>
-                    <img src="http://placehold.it/350x250" alt="..." class="">
-                    </a>
-                </div>
-            </div>
-
-            <!-- TH5 -->
-            <div class="col-sm-4">
-                <div class="thumbnail">
-                        <a href="detail.php?item_id=5" class="">
-                    <div class="caption">
-
-                        <p class="">
-                        </p>
-                    </div>
-                    <img src="http://placehold.it/350x250" alt="..." class="">
-                </a>
-                </div>
-            </div>
-
-
-            <!-- TH6 -->
-            <div class="col-sm-4">
-                <div class="thumbnail">
-                        <a href="detail.php?item_id=6" class="">
-                    <div class="caption">
-
-                        <p class="">
-                        </p>
-                    </div>
-                    <img src="http://placehold.it/350x250" alt="..." class="">
-                </a>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
-        <!-- end/row2 -->
+        <!-- ページ遷移部分 -->
+        <div aria-label="Page navigation">
+                    <ul class="pager">
+                        <?php if ($page == 1): ?>
+                            <!-- Newer押せない時 -->
+                            <!-- 最初のページより前は禁止 -->
+                            <li class="previous disabled">
+                                <a><span aria-hidden="true">&larr;</span> Newer
+                                </a>
+                            </li>
+                        <?php else: ?>
+                            <!-- Newer押せる時 -->
+                            <li class="previous">
+                                <a href="main.php?page=<?php echo $page - 1; ?>"><span aria-hidden="true">&larr;</span> Newer
+                                </a>
+                            </li>
+                        <?php endif; ?>
+
+                        <?php if ($page == $last_page): ?>
+                            <li class="next disabled">
+                                <a>Older <span aria-hidden="true">&rarr;</span>
+                                </a>
+                            </li>
+                        <?php else: ?>
+                            <li class="next">
+                                <a href="main.php?page=<?php echo $page + 1; ?>">Older <span aria-hidden="true">&rarr;</span>
+                                </a>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+        </div>
+
 
         <!-- 投稿エリア -->
         <section id="post" name="post">
             <div class="container">
                 <div class="row">
                         <div class="post">
-                            <div class="feed_form thumbnail">
-                                <form method="POST" action="">
+                            <div class="content_form thumbnail">
+
+                                <form method="POST" action="main.php" enctype="multipart/form-data">
+                                    <!-- 商品説明が入力されているか -->
                                     <div class="form-group" style="margin-bottom: 0px;">
-                                        <textarea name="feed" class="form-control" rows="2" placeholder="Your Comment Here" style="font-size: 24px; text-align: center;"></textarea><br>
-                                    <?php if(isset($errors['comment_id']) && $errors['comment_id'] == 'blank'): ?>
-                                        <p class="text-danger">写真を選択してください</p>         <?php endif; ?>
+                                        <textarea name="content" class="form-control" rows="2" placeholder="Your Comment Here" style="font-size: 24px; text-align: center;"></textarea><br>
+                                        <?php if (isset($errors['content'])&& $errors
+                                        ['content'] == 'blank'):?>
+                                            <p class="text-danger">文字を入力してください/ Can't be blank</p>
+                                        <?php endif; ?>
                                     </div>
+                                    <!-- 写真が選択されているか -->
                                     <div class="form-group">
-                                        <label for="img_name">Your Googs Image</label>
+                                        <label for="img_name">Your Goods Image</label>
                                         <input type="file" name="input_img_name" id="img_name" accept="image/*">
-
-
-
-                                    <?php if (isset($errors['comment/id'])&& $errors
-                                    ['comment_id'] == 'blank'):?>
-                                        <p class="text-danger">文字を入力してください</p>
-                        <?php endif; ?> 
+                                        <?php if(isset($errors['input_img_name']) && $errors['input_img_name'] == 'blank'): ?>
+                                            <p class="text-danger">写真を選択してください/ Please choose item image</p>
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="form-group" style="margin-bottom: 0px;">
+                                        <?php if(isset($errors['input_img_name']) && $errors['input_img_name'] == 'type'): ?>
+                                            <p class="text-danger">拡張子が違います/ Wrong file extension</p>
+                                        <?php endif; ?>
                                     </div>
                                     <input type="submit" value="POST (投稿する)" class="btn btn-primary">
                                 </form>
+
                             </div>
                     </div>
                 </div> <!-- /row -->
             </div> <!-- /container -->
         </section>
         <!-- /投稿エリア -->
-
-    </div><!--/row -->
-</div> <!-- end container -->
-
-        </div>
-    </div>
+        </div><!--/row -->
+    </div> <!-- end container -->
     
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </body>
 </html>
-
