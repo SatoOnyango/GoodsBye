abf93861f87f138de3aaade7871a520051e02110
佐藤秀嗣 <>
2018-12-18 19:56:06 +0800

    Edit dbconnect.php サーバーにあげるための変更（コメントアウト済み）

--- dbconnect.php
+++ dbconnect.php
@@ -5,3 +5,12 @@ $password='';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->query('SET NAMES utf8');
+
+
+// <?php
+// $dsn = 'mysql:dbname=goodsbye_test;host=mysql1010.db.sakura.ne.jp';
+// $user = 'goodsbye';
+// $password='batch-47';
+// $dbh = new PDO($dsn, $user, $password);
+// $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $dbh->query('SET NAMES utf8');
